e54494d452d51731c2388623a5f5c9cad9d6b0e5
Ripu Sudan Soni <ripu7481@gmail.com>
2021-06-17 21:07:22 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,9 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1 data-ns-test="project-name">ajkgjvhfhgj</h1>
-      <p  data-ns-test="project-description">yguybuiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiig
-      vfyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyjhgfvghjfjhgfffvfghbghbkjhjh hvgmbgh</p>
+      <h1 data-ns-test="project-name">Online Market</h1>
+      <p  data-ns-test="project-description">In this market you can buy or sold any thing via this website. and my teem comited to 
+           help any one who wantto do business via this website, If you need any help then mail to this compan. </p>
     </div>
   );
 }
